d855cac2db18b305abb59839a24243cb37913ff4
Denis Spiridonov <sdi@rshu.ru>
2015-05-05 14:31:42 +0300

    added parse_constraints method

--- src/pydap/handlers/grib/__init__.py
+++ src/pydap/handlers/grib/__init__.py
@@ -16,6 +16,13 @@ import struct
 import copy
 import pygrib
 
+def replace_name(pp_name):
+    pp_name = str(pp_name)
+    for ch in ': ()*%-':
+        pp_name = pp_name.replace(ch, '_')
+    return pp_name
+
+
 class GribHandler(BaseHandler):
     regexp = r"^.*\.(gr|grb|grib|gr1|grb1|grib1|gr2|grb2|grib2?|f\d{3}|anl)$"
     extensions = re.compile(regexp, re.IGNORECASE)
@@ -31,18 +38,22 @@ class GribHandler(BaseHandler):
 
 
         msg1 = self.grib.message(1)
-        _shape = msg1.data()[0].shape
+        self._shape = msg1.data()[0].shape
         _dim = ('lat', 'lon')
         _type = UInt16
+        self.lats, self.lons = msg1.latlons()
+        self.lats = self.lats[:, 0]
+        self.lons = self.lons[0]
 
         self.variables = []
         for i in range(self.grib.messages):
+            pp_name = replace_name(self.grib.message(i+1))
 
             self.variables.append(
                 BaseType(
-                    name=str(self.grib.message(i+1)),
+                    name=str(pp_name),
                     data=None,
-                    shape=_shape,
+                    shape=self._shape,
                     dimensions=_dim,
                     type=_type,
                     attributes={
@@ -52,7 +63,7 @@ class GribHandler(BaseHandler):
         lonVar = BaseType(
             name='lon',
             data=None,
-            shape=(_shape[1],),
+            shape=(self._shape[1],),
             dimensions=('lon',),
             type=Float32,
             attributes=({
@@ -67,7 +78,7 @@ class GribHandler(BaseHandler):
         latVar = BaseType(
             name='lat',
             data=None,
-            shape=(_shape[0],),
+            shape=(self._shape[0],),
             dimensions=('lat',),
             type=Float32,
             attributes=({
@@ -93,14 +104,71 @@ class GribHandler(BaseHandler):
 
             self.dataset[variable.name] = g
 
+    def get_data_for_parameter(self, pp_name, slices):
+        for i in range(self.grib.messages):
+            if replace_name(self.grib.message(i+1)) == pp_name:
+                if slices:
+                    return self.grib.message(i+1).data()[0][slices]
+                else:
+                    return self.grib.message(i+1).data()[0]
+
     def parse_constraints(self, environ):
+        dataset = copy.deepcopy(self.dataset)
+
         projection, selection = parse_qs(environ.get('QUERY_STRING', ''))
-
+        list_of_var = []
         if projection:
-            pass
-            # for name in self.f.files:
-            #     data = self.f[name][:]
-            #     dataset[name] = BaseType(name=name, data=data, shape=data.shape, type=data.dtype.char)
+            for var in projection:
+                var_name = var[len(var)-1][0]
+                slices = var[len(var)-1][1]
+                if var_name in list_of_var:
+                    continue
+                list_of_var.append(var_name)
+                if var_name in ['lat', 'lon']:
+                    if var_name == 'lon':
+                        if len(slices):
+                            lon_slice = slices[0]
+                        else:
+                            lon_slice = slice(0, self._shape[1])
 
-        dataset_copy = copy.deepcopy(self.dataset)
-        return dataset_copy
+                        print 'lon_slice = ', lon_slice
+                        data = self.lons[lon_slice]
+                        dataset[var_name] = BaseType(name=var_name,
+                                                     data=data,
+                                                     shape=data.shape,
+                                                     type=data.dtype.char)
+                    elif var_name == 'lat':
+                        if len(slices):
+                            lat_slice = slices[0]
+                        else:
+                            lat_slice = slice(0, self._shape[0])
+                        data = self.lats[lat_slice]
+                        dataset[var_name] = BaseType(name=var_name,
+                                                     data=data,
+                                                     shape=data.shape,
+                                                     type=data.dtype.char)
+                else:
+                    for variable in self.variables:
+                        if variable.name == var_name:
+                            if len(slices) != 2:
+                                slices = [slice(0, self._shape[0]), slice(0, self._shape[1])]
+                                # raise ValueError('Cannot obtain slices for %s. '
+                                #                  'Should be 3 slices, but %d found' % (var_name, len(slices)))
+                            print 'retrieving %s' % var_name, slices
+
+                            if slices:
+                                self.dataset[variable.name]['lat'].data = self.lats[slices[0]]
+                                self.dataset[variable.name]['lon'].data = self.lons[slices[1]]
+                            else:
+                                self.dataset[variable.name]['lat'].data = self.lats
+                                self.dataset[variable.name]['lon'].data = self.lons
+
+                            data = self.get_data_for_parameter(var_name, slices)
+                            dataset[var_name] = BaseType(name=var_name,
+                                                         data=data,
+                                                         shape=data.shape,
+                                                         type=data.dtype.char)
+                            # self.dataset[var_name].data = self.get_data_for_parameter(var_name, slices)
+                            break
+
+        return constrain(dataset, environ.get('QUERY_STRING', ''))
